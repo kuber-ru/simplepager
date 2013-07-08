790f77aefcbd8a52b11ed39760a48aebd0542d70
kuber <kuber@www.212.192.133.126>
2013-07-08 14:14:00 +0400

    Добавлено оформление в стиле Twitter Bootstrap

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
             per_page: 10,
             base_url : '',
             urlCountALL : '',
+            classActivePage: 'active',
             start : function(){},
             finish : function(){},
             pushQuery: function(){},
@@ -27,15 +28,18 @@
                base_url : settings.base_url,
                urlCountALL : settings.urlCountALL,
                page: settings.page,
-               total:  settings.total 
+               total:  settings.total,
+               classActivePage: settings.classActivePage
             }
         }
         function populateHTML(){
             pages = (settings.total - settings.total%settings.per_page)/settings.per_page
              html = '<ul>';
+             html += '<li class="notPage prev"><a href="#prev">' + 'Предыдущая' + '</a></li>';
                 for(var i = 1; i <= pages + 1; i++){
                     html += '<li>' + createLink(i) + '</li>';
                 }
+             html += '<li class="notPage next"><a href="#next">' + 'Следующая' + '</a></li>';
              html += '</ul>';
         return html;
         }
@@ -49,7 +53,7 @@
         
         function createLink(page){
            html = '';
-           html += '<a href="' + settings.base_url + '/?' + page + '">' + page + '</a>';
+           html += '<a href="#' + page + '">' + page + '</a>';
            return html;
         }
         
@@ -68,27 +72,54 @@
         }
         
         function setActivePage(){
-            $(settings.pages$).removeClass('active');
+            $(settings.pages$).removeClass(settings.classActivePage);
             $(settings.pages$).each(function(){
-                if($(this).text() == settings.page) $(this).addClass('active');
+                if($(this).text() == settings.page) $(this).addClass(settings.classActivePage);
             });
         }
 
           
         settings.start(this);
+        
+        
         $('body').on('click', '#pagination li',function(event){
+           
+            if ($(this).hasClass('notPage')) return;
+            
             event.preventDefault();
             settings.clickPage(getSettings());
             settings.page = $(settings.pages$[$(this).data('pager-index')]).text();
             getCountPages();
         });
         
+        $('body').on('click', '.notPage',function(event){
+           event.preventDefault();
+           pages = (settings.total - settings.total%settings.per_page)/settings.per_page;
+           if($(this).hasClass("prev")){
+               if (settings.page == 1) {
+                   settings.page =  pages + 1
+               } else {
+                   settings.page--;
+               }
+           }
+           if($(this).hasClass("next")){
+               if(settings.page ==  pages + 1){
+                   settings.page = 1;
+               } else {
+                   settings.page++;
+               }
+                
+           }
+           getCountPages();
+        });
+        
+       
+        
         getCountPages();
         settings.finish(this);
         
         return;
-       
-    }; 
+    };
 })(jQuery);
 
 
@@ -96,6 +127,7 @@ $(function(){
      $('#pagination').Pagination({
          urlCountALL: 'pagination.php',
          per_page: 10,
+         classActivePage: 'active',
          finish: function(){
            //  console.log("pager: Успешно создан!");
          },
